c93356810ebc4ad2cf2f5e0878f8753b00f7d349
Yuliya Muraveva <dju5@ya.ru>
2021-02-15 13:00:45 +0300

    change Vk Post

--- src/main/java/domain/CommentsInfo.java
+++ src/main/java/domain/CommentsInfo.java
@@ -3,4 +3,8 @@ package domain;
 public class CommentsInfo {
     private int commentsCount;
     private boolean canPost;
+    private boolean groupsCanPost;
+    private boolean canClose;
+    private boolean canOpen;
+
 }
---
+++ src/main/java/domain/Copyright.java
@@ -0,0 +1,8 @@
+package domain;
+
+public class Copyright {
+    private String id;
+    private String link;
+    private String name;
+    private String type;
+}
